ef0e507b5c84ff8d529451299d7b6502748e85fb
unknown <farizashakir13@gmail.com>
2020-11-21 14:34:37 +0530

    adding reset button

--- src/Hangman.js
+++ src/Hangman.js
@@ -20,6 +20,15 @@ class Hangman extends Component {
     super(props);
     this.state = { nWrong: 0, guessed: new Set(), answer: randomWord() };
     this.handleGuess = this.handleGuess.bind(this);
+    this.reset=this.reset.bind(this);
+  }
+
+  reset(){
+    this.setState=({
+      nWrong:0,
+      guessed:new Set(),
+      answer:randomWord
+    });
   }
 
   /** guessedWord: show current-state of word:
@@ -69,6 +78,7 @@ class Hangman extends Component {
         <p className='Hangman-btns'>
           {!gameOver? this.generateButtons():"You Lose"}
           </p>
+        <button id="reset"onClick={this.reset}>Reset</button>
       </div>
     );
   }
